28aa41812f478bbf3a8f29e7027ced7e6ad9d3ed
Divedevdone <125664463+Divedevdone@users.noreply.github.com>
2025-09-24 16:57:34 -0300

    Delete semed4 directory

--- semed4/popup.php
+++
@@ -1,8 +0,0 @@
-<div id="popup" class="popup">
-  <h3>Editar esta sessão</h3>
-  <form id="uploadForm" enctype="multipart/form-data">
-      <input type="file" name="arquivo" required>
-      <button type="submit">Enviar</button>
-  </form>
-  <button onclick="fecharPopup()">Cancelar</button>
-</div>
